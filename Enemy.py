881548f9b10d4ec4ce2a93714633dd97e65db099
williamlinnnn <linchen20050410.ee12@nycu.edu.tw>
2024-06-13 00:03:42 +0800

    different enemy

--- Enemy.py
+++ Enemy.py
@@ -2,12 +2,26 @@ import pygame
 import Robot
 
 class Enemy(Robot.Robot):
-    def __init__(self, x, y):
+    def __init__(self, x, y, enemytype):
         super().__init__(x, y)
-        self.image.fill((0, 0, 255))
-        self.speed = 0.1
         self.player_x = 400
         self.player_y = 300
+        self.enemytype = enemytype
+        if self.enemytype == 0:
+            self.health = 100
+            self.attack = 10
+            self.image.fill((255, 0, 0))
+            self.speed = 0.3
+        elif self.enemytype == 1:
+            self.health = 40
+            self.attack = 20
+            self.image.fill((0, 255, 0))
+            self.speed = 0.5
+        else:  
+            self.health = 150
+            self.attack = 25
+            self.image.fill((0, 0, 255))
+            self.speed = 0.1
     def update_player_position(self, player_x, player_y):
         self.player_x = player_x
         self.player_y = player_y
