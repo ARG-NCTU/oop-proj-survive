3109e8d574cdc33a542495aafe53bf953626102c
unknown <williamlin20050410@gmail.comgit config --global user.name Williamlinnnn>
2024-06-12 18:09:47 +0800

    enemy track player

--- Enemy.py
+++ Enemy.py
@@ -2,11 +2,25 @@ import pygame
 import Robot
 
 class Enemy(Robot.Robot):
-    def __init__(self, x, y):
+    def __init__(self, x, y,player_x, player_y):
         super().__init__(x, y)
         self.image.fill((0, 0, 255))
         self.speed = 2
+        self.player_x = player_x
+        self.player_y = player_y
+    def update_player_position(self, player_x, player_y):
+        self.player_x = player_x
+        self.player_y = player_y
     def update(self):
-        pass
+        robot_x = self.player_x
+        robot_y = self.player_y
+        if robot_x < self.rect.x:
+            self.rect.x -= self.speed
+        if robot_x > self.rect.x:
+            self.rect.x += self.speed
+        if robot_y < self.rect.y:
+            self.rect.y -= self.speed
+        if robot_y > self.rect.y:
+            self.rect.y += self.speed
     def draw(self, screen):
         super().draw(screen)
