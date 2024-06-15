bba59e20b95b113f5f0feb512730c0753867745a
benjaminpot <benjaminkuo67@gmail.com>
2024-06-15 21:50:40 +0800

    Refactor gun level upgrade logic in Player class

--- Player.py
+++ Player.py
@@ -34,9 +34,9 @@ class Player(Robot2.Robot2):
         self.bullet_cooldown = 5
         self.bullet_cooldown_max = 7
         self.ready_to_shoot = True
-        self.barrel_amout = [1,2,4,5,6,7,8]
         self.superlevel = 1
 
+        self.barrel_amout = [1,2,4,5,6,7,8]
         self.gunlevel_to_bullet = {
             "bullet_speed":[1,0,0,1,0,0],
             "bullet_reload":[0,-1,0,-1,0,-1],
@@ -208,22 +208,6 @@ class Player(Robot2.Robot2):
     def add_gun_level(self):
         self.talent_point -= 1
         self.gun_level += 1
-    def reset(self):
-        self.speed = 50
-        self.max_speed = 500
-        self.health = 200
-        self.level = 1
-        self.max_health = 200 
-
-        self.attack = 30
-        self.max_bullets = 10
-        self.bullets = 10
-        self.bullet_speed = 10
-        self.bullet_reload = 30
-        self.bullet_reload_max = 35
-        self.bullet_cooldown = 5
-        self.bullet_cooldown_max = 7
-        self.ready_to_shoot = True
 
         self.bullet_speed += self.gunlevel_to_bullet["bullet_speed"][(self.gun_level-1)%6]
         self.bullet_reload += self.gunlevel_to_bullet["bullet_reload"][(self.gun_level-1)%6]
@@ -237,3 +221,26 @@ class Player(Robot2.Robot2):
         # self.bullet_cooldown_max = max(0, self.bullet_cooldown_max)
         # self.bullet_speed += 1
         # self.max_bullets += 1
+
+    def reset(self):
+        self.speed = 50
+        self.max_speed = 500
+        self.health = 200
+        self.level = 1
+        self.max_level = 30
+        self.max_health = 200 
+        self.gun_level = 1
+        self.talent_point = 2
+
+        self.attack = 30
+        self.max_bullets = 10
+        self.bullets = 10
+        self.bullet_speed = 10
+        self.bullet_reload = 30
+        self.bullet_reload_max = 35
+        self.bullet_cooldown = 5
+        self.bullet_cooldown_max = 7
+        self.ready_to_shoot = True
+        self.superlevel = 1
+
+        
